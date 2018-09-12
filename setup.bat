52c6ff077ca9e0b980ec6e64ee947b97c73dffd5
Ryan Couch <rcouch00@gmail.com>
2018-09-12 09:05:06 -0500

    removing azure and setup settings

--- setup.bat
+++
@@ -1,6 +0,0 @@
-echo "# LeftReact" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/rcouch00/LeftReact.git
-git push -u origin master
